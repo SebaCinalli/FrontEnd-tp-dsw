4384104b156e085ee940c8680b5156d4ea9a25c6
nanosalvu11 <salvuccinahuel@gmail.com>
2025-08-06 23:46:20 -0300

    implementacion en el front para mantener la sesion iniciada al recargar la pagina, usando un token y peticiones axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes/routes";
 import "./App.css";
+import { useEffect } from "react";
+import { useUser } from "./context/usercontext";
 
 function App() {
+  const { checkToken } = useUser();
+
+  useEffect(() => {
+    checkToken();
+  }, []);
+
   return (
     <Router>
       <AppRoutes />
